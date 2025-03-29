43714422f341daea0276c113122ec12a3b0c3170
Jibaru <ignacioruedaboada@hotmail.com>
2025-03-29 15:59:37 -0500

    fix: handle non-repeated app key

--- internal/scripts/create_app.go
+++ internal/scripts/create_app.go
@@ -2,6 +2,7 @@ package scripts
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -29,6 +30,15 @@ func NewCreateAppScript(db *mongo.Database) *CreateAppScript {
 }
 
 func (s *CreateAppScript) Exec(ctx context.Context, req CreateAppReq) (*CreateAppResp, error) {
+	existing, err := persistence.GetAppByKey(ctx, s.db, req.AppKey)
+	if err != nil && !errors.Is(err, mongo.ErrNoDocuments) {
+		return nil, err
+	}
+
+	if existing != nil {
+		return nil, errors.New("app with the provided app key already exists")
+	}
+
 	app := persistence.App{
 		ID:        primitive.NewObjectID(),
 		AppKey:    req.AppKey,
@@ -36,7 +46,7 @@ func (s *CreateAppScript) Exec(ctx context.Context, req CreateAppReq) (*CreateAp
 		UserID:    req.UserID,
 		CreatedAt: time.Now().UTC(),
 	}
-	err := persistence.SaveApp(ctx, s.db, app)
+	err = persistence.SaveApp(ctx, s.db, app)
 	if err != nil {
 		return nil, err
 	}
